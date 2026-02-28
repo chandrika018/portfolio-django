08f17e16ef2c46ae330f2193ca5a07e9d9f909a2
Chandrika Nandehariya <chandrikanandesariya018@gmail.com>
2026-02-28 23:12:46 +0530

    convert home page into responsive design

--- firstdjango/settings.py
+++ firstdjango/settings.py
@@ -117,10 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # added manualy
 
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR / "static",)
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -11,3 +13,6 @@ urlpatterns = [
     path('contact',views.contact,name='contact')
    
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
