2b30bac2db4bdb664d9754043d18d48f286c0965
Chandrika Nandehariya <chandrikanandesariya018@gmail.com>
2026-03-03 21:41:20 +0530

    update database

--- home/models.py
+++ home/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 class Contact(models.Model):
     name = models.CharField(max_length=122)
-    email = models.CharField(max_length=122)
-    phone = models.CharField(max_length=12)
-    desc = models.TextField()
-   
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
 
 def __str__(self):
     return self.name
